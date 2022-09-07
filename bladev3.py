d99db64b09e7fa5d6435d626d6bf6c5559fbf866
kontoIID <85289663+kontoI@users.noreply.github.com>
2022-09-07 19:48:27 +0700

    Add files via upload

--- bladev3.py
+++ bladev3.py
@@ -511,11 +511,6 @@ def otomatis():
 						except:pwx.append(nama)
 				else:
 					pwx.append(nama)
-					pwx.append(sayang)
-					pwx.append(anjing)
-					pwx.append(kontol)
-					pwx.append(anjing)
-					pwx.append(bismillah)
 					pwx.append(depan+"123")
 					pwx.append(depan+"12345")
 			if 'mobile' in metode:
